831ebeb8e376c10f2db0988364b47365a195a70a
Aaron Wiens <aaron@awwebsolutions.com>
2020-05-29 16:06:00 -0500

    Updated navigation bars.

---
+++ src/SmartBudget.WPF/Controls/TopBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SmartBudget.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for TopBar.xaml
+    /// </summary>
+    public partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
