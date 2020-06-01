2f9d2e4fb1478d22cd118a77544e098137ae9822
Aaron Wiens <amwiens@hotmail.com>
2020-05-31 22:12:47 -0500

    Added prism and created a region.

--- src/SmartBudget.WPF/App.xaml.cs
+++ src/SmartBudget.WPF/App.xaml.cs
@@ -1,11 +1,25 @@
-﻿using System.Windows;
+﻿using Prism.Ioc;
+using Prism.Unity;
+
+using SmartBudget.WPF.Views;
+
+using System.Windows;
 
 namespace SmartBudget.WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
