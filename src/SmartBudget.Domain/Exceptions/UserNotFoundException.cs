14809cf656de796cc4cf78fbe6361a96b1d5d722
Aaron Wiens <amwiens@hotmail.com>
2020-06-04 23:36:33 -0500

    Added exceptions.

---
+++ src/SmartBudget.Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SmartBudget.Domain.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public string Username { get; set; }
+
+        public UserNotFoundException(string username)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string username, string message)
+            : base(message)
+        {
+            Username = username;
+        }
+
+        public UserNotFoundException(string username, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            Username = username;
+        }
+    }
+}
