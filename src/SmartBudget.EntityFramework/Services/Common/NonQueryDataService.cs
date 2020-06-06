4450a7d7121848f1596b4b7e5772121f4f3aa008
Aaron Wiens <amwiens@hotmail.com>
2020-06-05 21:56:15 -0500

    Added services.

---
+++ src/SmartBudget.EntityFramework/Services/Common/NonQueryDataService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+using SmartBudget.Domain.Models;
+
+using System.Threading.Tasks;
+
+namespace SmartBudget.EntityFramework.Services.Common
+{
+    public class NonQueryDataService<T> where T : DomainObject
+    {
+        private readonly SmartBudgetDbContextFactory _contextFactory;
+
+        public NonQueryDataService(SmartBudgetDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+        public async Task<T> Update(int id, T entity)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+    }
+}
---
+++ src/SmartBudget.EntityFramework/Services/UserDataService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SmartBudget.Domain.Models;
+using SmartBudget.Domain.Services;
+using SmartBudget.EntityFramework.Services.Common;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartBudget.EntityFramework.Services
+{
+    public class UserDataService : IUserService
+    {
+        private readonly SmartBudgetDbContextFactory _contextFactory;
+        private readonly NonQueryDataService<User> _nonQueryDataService;
+
+        public UserDataService(SmartBudgetDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+            _nonQueryDataService = new NonQueryDataService<User>(contextFactory);
+        }
+
+        public async Task<User> Create(User entity)
+        {
+            return await _nonQueryDataService.Create(entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _nonQueryDataService.Delete(id);
+        }
+
+        public async Task<User> Get(int id)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                User entity = await context.Users
+                    .FirstOrDefaultAsync(u => u.Id == id);
+                return entity;
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                IEnumerable<User> entities = await context.Users
+                    .ToListAsync();
+                return entities;
+            }
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                return await context.Users
+                    .FirstOrDefaultAsync(u => u.Email == email);
+            }
+        }
+
+        public async Task<User> GetByUsername(string username)
+        {
+            using (SmartBudgetDbContext context = _contextFactory.CreateDbContext())
+            {
+                return await context.Users
+                    .FirstOrDefaultAsync(u => u.Username == username);
+            }
+        }
+
+        public async Task<User> Update(int id, User entity)
+        {
+            return await _nonQueryDataService.Update(id, entity);
+        }
+    }
+}
