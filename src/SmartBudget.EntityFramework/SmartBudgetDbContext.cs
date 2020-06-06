0ab9ac4385e906a99bc30e64db3c7a92074a4a9a
Aaron Wiens <amwiens@hotmail.com>
2020-06-05 21:29:51 -0500

    Added DbContext.

---
+++ src/SmartBudget.EntityFramework/SmartBudgetDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SmartBudget.Domain.Models;
+
+namespace SmartBudget.EntityFramework
+{
+    public class SmartBudgetDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        //public DbSet<Transaction> Transactions { get; set; }
+        //public DbSet<Expense> Expenses { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+
+        public SmartBudgetDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Category>().HasMany(a => a.Transactions);
+
+            //modelBuilder.Entity<Transaction>().HasMany(a => a.Categories);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/SmartBudget.EntityFramework/SmartBudgetDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SmartBudget.EntityFramework
+{
+    public class SmartBudgetDbContextFactory : IDesignTimeDbContextFactory<SmartBudgetDbContext>
+    {
+        public SmartBudgetDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<SmartBudgetDbContext>();
+            options.UseSqlServer(@"Server=localhost;Database=SmartBudgetDB1;Trusted_Connection=True;");
+
+            return new SmartBudgetDbContext(options.Options);
+        }
+    }
+}
